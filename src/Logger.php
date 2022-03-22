0d74c3e829586749490ce45e452e8473fac60de3
Nat Okpe <natokpe@gmail.com>
2022-03-22 18:35:02 +0100

    Added logger

---
+++ src/Logger.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+
+namespace natokpe\Guib;
+
+/**
+ * 
+ */
+class Logger
+{
+    /**
+     * 
+     */
+    public static
+    function warning()
+    {
+        // 
+    }
+
+    /**
+     * alias of Logger::warning()
+     */
+    public static
+    function warn()
+    {
+        // 
+    }
+
+    /**
+     * 
+     */
+    public static
+    function error()
+    {
+        // 
+    }
+}
